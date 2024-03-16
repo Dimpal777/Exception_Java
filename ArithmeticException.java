5bd9e94e87b1e64bffb55d149f3783db94607599
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2024-03-16 17:00:26 +0530

    Add files via upload

---
+++ ArithmeticException.java
@@ -0,0 +1,11 @@
+public class ArithmeticException {
+    public static void main(String[] args) {
+        int dividend = 10;
+        int divisor = 5;
+        
+        // Attempting to divide by zero
+        int result = dividend / divisor;
+        
+        System.out.println("Result: " + result);
+    }
+}
